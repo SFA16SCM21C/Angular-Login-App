984a23d7b07e02088e1a0e09a3177aedcb9c8b4c
Saloni Chauhan <schauhan1@hawk.iit.edu>
2023-07-23 14:05:15 -0500

    service changes and enviornment changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //new components for the project
 import { LoginComponent } from './login/login.component';
+
+//new services created for the project
 import { AuthServiceService } from './auth-service.service';
+
+//new modules used for the project
 import { HttpClientModule } from "@angular/common/http"
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +25,8 @@ import { HttpClientModule } from "@angular/common/http"
     AppRoutingModule,
     BrowserAnimationsModule,
     AuthServiceService,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { baseUrl } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,8 @@ export class AuthServiceService {
   constructor(private http: HttpClient) { }
 
   //login method created to check the credentils and make the requests
+
   login(data): Observable<any> {
-    return this.http.post('users/login', data);
+    return this.http.post(`${baseUrl}users/login`, data);
   }
 }
