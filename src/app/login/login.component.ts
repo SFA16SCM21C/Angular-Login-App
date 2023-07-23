9ddca50b689a1fa5d729b7db220e024fb443cfa3
Saloni Chauhan <schauhan1@hawk.iit.edu>
2023-07-23 14:37:25 -0500

    created formgroup and formcontrols

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
+  formGroup: FormGroup
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  initForm () {
+    this.formGroup = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
 }
